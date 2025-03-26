632c5b84ba46c97b7fa902c00334a81f3e4bf2d4
Arin Ahmed <arin2023b@gmail.com>
2025-03-26 11:34:10 +0600

    Blog App and Blog Model

--- Blog_main/urls.py
+++ Blog_main/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+# nichar 2 ta import kra hoise media file ke urlpatterns er sthe concatinate krar jnno
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-]
+] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ blogs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category,Blog
+
+# nichar class ta autometically slug generate krar jnno create kra hoise
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title','category','author','status','is_featured')
+    search_fields = ('id','title','category__category_name','status') # models.py a jei sob filed a ForeignKey use kra hoisilo oigulke direct use kra jbe na
+    # tai category te __category_name use kra hoise
+    list_editable = ('is_featured',)
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin) # ekhne BlogAdmin class ta pass kre daoa hoise
